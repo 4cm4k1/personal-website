0a1d1a3fb6648856632ce06b7a53a3ab053959ab
Anthony Maki <4cm4k1@gmail.com>
2016-11-17 17:43:13 -0600

    Switch to Angular2, AngularMaterial2, Angularfire2

--- client/scripts/_appDeclaration.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  'use strict'
-
-  var dependencies = ['ngMaterial', 'ngMessages', 'firebase', 'ngRoute']
-
-  angular.module('anthonyCodes', dependencies)
-})()
--- public/assets/vendors/angularfire/index.js
+++
@@ -1,9 +0,0 @@
-// Make sure dependencies are loaded on the window
-require('angular');
-require('firebase');
-
-// Load the Angular module which uses window.angular and window.Firebase
-require('./dist/angularfire');
-
-// Export the module name from the Angular module
-module.exports = 'firebase';
