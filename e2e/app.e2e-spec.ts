33130a10b89b8c195cda284b617664cf37ef14f5
Anthony Maki <4cm4k1@gmail.com>
2017-03-27 16:24:33 -0500

    More updates

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AnthonyCodesPage } from './app.po';
+import {AnthonyCodesPage} from './app.po';
 
 describe('anthony-codes App', () => {
-  let page: AnthonyCodesPage;
+	let page: AnthonyCodesPage;
 
-  beforeEach(() => {
-    page = new AnthonyCodesPage();
-  });
+	beforeEach(() => {
+		page = new AnthonyCodesPage();
+	});
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('ac works!');
-  });
+	it('should display message saying app works', () => {
+		page.navigateTo();
+		expect(page.getParagraphText()).toEqual('app works!');
+	});
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'ac-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ac works!';
+	title = 'app works!';
 }
