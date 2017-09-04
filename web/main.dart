1e4e0715853d2e9a58658330a40c857255ef4fb1
Anthony Maki <4cm4k1@gmail.com>
2017-09-04 00:09:08 -0500

    Changed project to use Dart and AngularDart

---
+++ web/main.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2017, Anthony Maki. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular/angular.dart';
+
+import 'package:anthony_codes/app_component.dart';
+
+void main() {
+  bootstrap(AppComponent);
+}
