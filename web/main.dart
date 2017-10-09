06072c5ec609ecefb74726a6a1794d9e4c6c31ab
Anthony Maki <4cm4k1@gmail.com>
2017-10-09 15:53:54 -0500

    Change package/repo name to website

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,6 @@
 import 'package:angular/angular.dart';
 
-import 'package:anthony_codes/app_component.dart';
+import 'package:website/app_component.dart';
 
 void main() {
   bootstrap(AppComponent);
