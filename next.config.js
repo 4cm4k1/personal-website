feaf1b740e711940d9c535744e15c4aaa934c6c5
Anthony Maki <4cm4k1@gmail.com>
2018-11-04 23:16:22 -0600

    Add next-optimized-images for juicy multiformat base64 inlining goodness

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 const withMDX = require('@zeit/next-mdx')(); // see https://github.com/zeit/next-plugins/issues/231#issuecomment-433587758
 const withOffline = require('next-offline');
+const withOptimizedImages = require('next-optimized-images');
 const withPlugins = require('next-compose-plugins');
 const withSourceMaps = require('@zeit/next-source-maps');
 
@@ -50,6 +51,7 @@ module.exports = withPlugins(
     withBundleAnalyzer(bundleAnalyzerConfig),
     withMDX,
     withOffline,
+    withOptimizedImages,
     withSourceMaps,
   ],
   nextConfig,
