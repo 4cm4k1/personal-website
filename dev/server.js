0c00bc0fdb84f19f01d609d06d99430a04f038ba
Anthony Maki <4cm4k1@gmail.com>
2018-11-29 22:33:35 -0600

    Add sleep function with JSDoc comment to server.js to avoid proxy server being ready before next dev is

--- dev/server.js
+++ dev/server.js
@@ -1,15 +1,26 @@
 const { readFileSync } = require('fs');
 const httpProxy = require('http-proxy');
 
-httpProxy
-  .createServer({
-    target: {
-      host: 'localhost',
-      port: 3000,
-    },
-    ssl: {
-      key: readFileSync('./dev/key.pem'),
-      cert: readFileSync('./dev/cert.pem'),
-    },
-  })
-  .listen(443);
+/**
+ * Idles main thread for given amount of time
+ * @param {number} time Time to pause in milliseconds
+ * @returns {Promise} Promise that resolves after setTimeout
+ */
+function sleep(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+sleep(5000).then(() =>
+  httpProxy
+    .createServer({
+      target: {
+        host: 'localhost',
+        port: 3000,
+      },
+      ssl: {
+        key: readFileSync('./dev/key.pem'),
+        cert: readFileSync('./dev/cert.pem'),
+      },
+    })
+    .listen(443),
+);
