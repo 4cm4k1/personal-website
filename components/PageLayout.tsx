99f1cce3820c4f417495225665ad417ace107d4e
Anthony Maki <4cm4k1@gmail.com>
2019-07-05 00:44:45 -0500

    Upgrades some dependencies, refactors AMP according to Next API change

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -1,6 +1,5 @@
 import { Cell, Grid, Row } from '@material/react-layout-grid';
 import constants from '../lib/constants';
-import { withAmp } from 'next/amp';
 
 const { assetPath } = constants;
 
@@ -8,19 +7,18 @@ interface Props {
   children: React.ReactNode;
 }
 
-export default withAmp(
-  ({ children }: Props) => (
-    <Grid>
-      <Row>
-        <Cell desktopColumns={2} tabletColumns={1} />
-        <Cell desktopColumns={8} tabletColumns={6} phoneColumns={4}>
-          {children}
-        </Cell>
-        <Cell desktopColumns={2} tabletColumns={1} />
-      </Row>
-    </Grid>
-  ),
-  { hybrid: true },
+export default ({ children }: Props) => (
+  <Grid>
+    <Row>
+      <Cell desktopColumns={2} tabletColumns={1} />
+      <Cell desktopColumns={8} tabletColumns={6} phoneColumns={4}>
+        {children}
+      </Cell>
+      <Cell desktopColumns={2} tabletColumns={1} />
+    </Row>
+  </Grid>
 );
 
 export { assetPath };
+
+export const config = { amp: 'hybrid' };
