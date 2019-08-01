0a8e11d1369a6cfa6539428b07bf4a24ca5354cb
Anthony Maki <4cm4k1@gmail.com>
2019-07-31 21:23:46 -0500

    Upgrades dependencies, removes default `next-offline` config in `next.config.js`

--- next.config.js
+++ next.config.js
@@ -16,35 +16,10 @@ const sass = require('sass'),
       publicDirectory: true,
       modern: true,
     },
-    generateInDevMode: true,
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-    // registerSwPrefix: '_next/static', // TODO: see below
-    publicRuntimeConfig: false, // temp workaround
     target: 'serverless',
     webpack: (config, { buildId, dev, isServer, defaultLoaders }) => config,
     webpackDevMiddleware: config => config,
-    workboxOpts: {
-      // importsDirectory: 'static',
-      // importWorkboxFrom: 'local', TODO: use 'local' but without duplicate `_next/static/static/`
-      swDest: 'static/service-worker.js',
-      runtimeCaching: [
-        {
-          urlPattern: /^https?.*/,
-          handler: 'networkFirst',
-          options: {
-            cacheName: 'https-calls',
-            networkTimeoutSeconds: 15,
-            expiration: {
-              maxEntries: 150,
-              maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-            },
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
-      ],
-    },
   },
   sassConfig = {
     sassLoaderOptions: {
