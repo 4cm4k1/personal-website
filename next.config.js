4a5ae6bfc128e5295f0fdaa928f80010a631de91
Anthony Maki <4cm4k1@gmail.com>
2020-02-09 02:30:32 -0600

    refactor: Moves `redirects`, `rewrites`, and `headers` to `next.config.js`, adds `strict-dynamic` CSP, upgrades dependencies, adds experimental Next.js config

--- next.config.js
+++ next.config.js
@@ -7,18 +7,123 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   withSourceMaps = require('@zeit/next-source-maps')(),
   nextConfig = {
     experimental: {
+      catchAllRouting: true,
+      deferScripts: true,
+      granularChunks: true,
       modern: true,
       pages404: true,
       plugins: true,
       polyfillsOptimization: true,
+      prefetchPreload: true,
       workerThreads: true,
+      async redirects() {
+        return [
+          {
+            source: '/source{/}?',
+            statusCode: 301,
+            destination: '/_src',
+          },
+          {
+            source: '/logs{/}?',
+            statusCode: 301,
+            destination: '/_logs',
+          },
+        ];
+      },
+      async rewrites() {
+        return [
+          {
+            source: '/feed.(atom|json|xml)',
+            destination: '/_next/static/feed.$1',
+          },
+          {
+            source: '/keybase.txt',
+            destination: '/.well-known/keybase.txt',
+          },
+          {
+            source: '/service-worker.js',
+            destination: '/_next/static/service-worker.js',
+          },
+        ];
+      },
+      async headers() {
+        return [
+          {
+            source: '/service-worker.js',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'public, max-age=43200, immutable',
+              },
+              {
+                key: 'Service-Worker-Allowed',
+                value: '/',
+              },
+            ],
+          },
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Access-Control-Allow-Origin',
+                value: 'https://anthony.codes',
+              },
+              {
+                key: 'Cache-Control',
+                value: 'public, max-age=31536000, immutable',
+              },
+              {
+                key: 'Feature-Policy',
+                value: `accelerometer 'none'; autoplay 'none'; camera 'none'; fullscreen 'self'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; midi 'none'; payment 'none'; speaker 'self'; sync-xhr 'self'; usb 'none'; vr 'none'`,
+              },
+              {
+                key: 'Referrer-Policy',
+                value: 'strict-origin-when-cross-origin',
+              },
+              {
+                key: 'Strict-Transport-Security',
+                value: 'max-age=63072000; includeSubDomains; preload',
+              },
+              {
+                key: 'X-Content-Type-Options',
+                value: 'nosniff',
+              },
+              {
+                key: 'X-DNS-Prefetch-Control',
+                value: 'on',
+              },
+              {
+                key: 'X-Download-Options',
+                value: 'noopen',
+              },
+              {
+                key: 'X-Frame-Options',
+                value: 'DENY',
+              },
+              {
+                key: 'X-Permitted-Cross-Domain-Policies',
+                value: 'none',
+              },
+              {
+                key: 'X-UA-Compatible',
+                value: 'IE=edge; chrome=1',
+              },
+              {
+                key: 'X-XSS-Protection',
+                value: '1; mode=block; report=/report-xss-violation',
+              },
+            ],
+          },
+        ];
+      },
     },
     future: {
       excludeDefaultMomentLocales: true,
     },
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    reactStrictMode: true,
     target: 'serverless',
-    webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+    webpack: (config, { dev, isServer }) => {
       const splitChunks =
         config.optimization && config.optimization.splitChunks;
       if (splitChunks) {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,28 @@
+import crypto from 'crypto';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
+const cspHashOf = (text: any) => {
+  const hash = crypto.createHash('sha256');
+  hash.update(text);
+  return `'sha256-${hash.digest('base64')}'`;
+};
+
+export default class extends Document {
   static async getInitialProps(ctx: any) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
 
   render() {
+    const csp = `upgrade-insecure-requests; object-src 'none'; script-src ${cspHashOf(
+      NextScript.getInlineScriptSource(this.props),
+    )} 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https:; base-uri 'none'; report-uri /report-csp-violation`;
+
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta httpEquiv='Content-Security-Policy' content={csp} />
+        </Head>
         <body>
           <Main />
           <NextScript />
@@ -18,5 +31,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
