602ac557a471c559156974e3f95a69a870644b64
Anthony Maki <4cm4k1@gmail.com>
2023-02-21 05:14:37 +0000

    Update landing page to match revised résumé style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,71 +1,65 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-// import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>anthony.app</title>
-        <meta
-          name='description'
-          content='web software engineer in Minneapolis'
-        />
+        <title>anthony maki @ anthony.app</title>
+        <meta name='description' content='remote web engineer in minneapolis' />
       </Head>
 
-      <main className={styles.container}>
-        <h1 className={styles.header}>welcome 👋</h1>
-        <p className={styles.text}>
-          Anthony’s a Minneapolis-based remote software engineer (he/they) who
-          traffics in high-level languages (e.g., Node, JavaScript, TypeScript),
-          serverless/full-stack framework-based web apps (e.g., λ functions,
-          Next.js, React, Svelte), &amp; design systems (e.g., a11y, Material
-          Design, Bootstrap, tailwind.css).
+      <main>
+        <h1 className={styles.header}>anthony maki</h1>
+        <h2 className={styles.subheader}>remote web engineer</h2>
+        <p className={styles.bio}>
+          he/they • minneapolis •{' '}
+          <a className={styles.link} href='https://anthony.app'>
+            https://anthony.app
+          </a>{' '}
+          •{' '}
+          <a className={styles.link} href='mailto:4cm4k1@gmail.com'>
+            4cm4k1@gmail.com
+          </a>{' '}
+          •{' '}
+          <a className={styles.link} href='tel:+16122089588'>
+            +1 612–208–9588
+          </a>
         </p>
-        <p className={styles.text}>
-          He maintains many hats as a docs-writer, library-builder,
-          UX/DX-improver, CI/CD-automator, scripter, git-hooker, &amp;
-          open-source contributor.
-        </p>
-        <p className={styles.text}>
-          All in pursuit of building human-centered software.
-        </p>
+        <ul className={styles.list}>
+          <li>
+            <a className={styles.link} href='/resume'>
+              résumé
+            </a>
+          </li>
+          <li>
+            <a className={styles.link} href='https://github.com/4cm4k1'>
+              github
+            </a>
+          </li>
+          <li>
+            <a className={styles.link} href='https://linkedin.com/in/4cm4k1'>
+              linkedin
+            </a>
+          </li>
+          <li>
+            <a
+              className={styles.link}
+              rel='me'
+              href='https://mastodon.social/@4cm4k1'
+            >
+              mastodon
+            </a>
+          </li>
+          <li>
+            <a className={styles.link} href='https://twitter.com/4cm4k1'>
+              twitter
+            </a>
+          </li>
+        </ul>
       </main>
-      <ul className={styles.list}>
-        <li>
-          <a className={styles.link} href='/resume'>
-            résumé
-          </a>
-        </li>
-        <li>
-          <a className={styles.link} href='https://github.com/4cm4k1'>
-            github
-          </a>
-        </li>
-        <li>
-          <a
-            className={styles.link}
-            rel='me'
-            href='https://mastodon.social/@4cm4k1'
-          >
-            mastodon
-          </a>
-        </li>
-      </ul>
-      {/* <footer className={styles.footer}>
-        <span className={styles.text}>
-          powered by{' '}
-          <span className={styles.logo}>
-            <Image
-              src='/icon-32x32.png'
-              alt='anthony.app'
-              width={16}
-              height={16}
-            />
-          </span>
-        </span>
-      </footer> */}
     </>
   );
 };
